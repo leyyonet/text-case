87ddb53afd4bf882967bd69141305569575e6e11
Mustafa Yelmer <mustafayelmer@gmail.com>
2026-03-18 05:26:47 +0300

    refactor cli

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,14 @@
 # @leyyo/text-case
+> Don't touch
+-------
 
-## 4.0.2
+## Changelog
 
-### Patch Changes
+| Date                | Version | User           | Message      |
+|---------------------|---------|----------------|--------------|
+| 2026-03-18 02:26:47 | 4.0.3   | Mustafa Yelmer | refactor cli |
 
-- minify+changeset
+-------
+- checksum : *debf00c9061d2f516262695a7171b71bd46d2cc9a86068a7e264ecbee6dac63e*
+- time     : *2026-03-18 02:26:47*
+- builder  : *leyyo*
--- eslint.config.js
+++ eslint.config.js
@@ -4,32 +4,32 @@ import nodePlugin from "eslint-plugin-n";
 import prettier from "eslint-config-prettier";
 
 export default [
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    prettier,
-    {
-        files: ["**/*.ts"],
-        languageOptions: {
-            parserOptions: {
-                project: true,
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-    {
-        plugins: {
-            n: nodePlugin,
-        },
-        rules: {
-            "n/no-missing-import": 'off', // NodeNext ile çakışabiliyor
-            "@typescript-eslint/no-unused-vars": 'off',
-            "@typescript-eslint/no-namespace": 'off',
-            "@typescript-eslint/prefer-namespace-keyword": 'off',
-            "@typescript-eslint/no-misused-new": 'off',
-            "@typescript-eslint/ban-types": 'off',
-            "@typescript-eslint/no-explicit-any": 'off',
-            "@typescript-eslint/no-unsafe-function-type": 'off',
-            "@typescript-eslint/no-empty-object-type": 'off',
-        },
-    }
+  },
+  {
+    plugins: {
+      n: nodePlugin,
+    },
+    rules: {
+      "n/no-missing-import": "off", // NodeNext ile çakışabiliyor
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/prefer-namespace-keyword": "off",
+      "@typescript-eslint/no-misused-new": "off",
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
 ];
